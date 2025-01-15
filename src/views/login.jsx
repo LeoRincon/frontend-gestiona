79c096bd8058537183724a6f41194f1dff53d7df
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-15 15:19:54 -0500

    Add login styles and structure to enhance user interface

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,17 +1,24 @@
 import { AuthHeader } from "./components/AuthHeader";
 import { FormLogin } from "./components/FormLogin";
 
+import "./login.css";
+
 export const Login = () => {
   return (
-    <div className="container">
+    <div className="view">
+      <div className="container">
         <AuthHeader />
         <main className="container__cardform">
-            <h1 className="cardform__title">Inicia Sesión</h1>
-            <FormLogin />
-            <p className="cardform__text">¿No tienes una cuenta?
-                <a href="./sign-up.html" className="card_form__text--link">Registrate</a>
-            </p>
+          <h1 className="cardform__title">Inicia Sesión</h1>
+          <FormLogin />
+          <p className="cardform__text">
+            ¿No tienes una cuenta?
+            <a href="./sign-up.html" className="card_form__text--link">
+              Registrate
+            </a>
+          </p>
         </main>
+      </div>
     </div>
-  )
-}
+  );
+};
