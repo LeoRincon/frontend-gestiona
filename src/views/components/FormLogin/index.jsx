2dd6d5822dabeaf24c26ff9fd0cd7264c9ece4e4
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-15 15:18:41 -0500

    Add FormLogin component with styles and localized labels

--- src/views/components/FormLogin/index.jsx
+++ src/views/components/FormLogin/index.jsx
@@ -1,14 +1,37 @@
+import "./styles.css";
 
 export const FormLogin = () => {
+  const email = "Correo";
+  const password = "Contraseña";
+  const login = "Iniciar Sesión";
+
   return (
     <form className="cardform__form">
-      <label className="form__label--required" htmlFor="email">Correo</label>
-      <input className="form__input" type="email" id="email" name="email" placeholder="Ingrese su correo"
-          required />
-      <label className="form__label--required" htmlFor="password">Contraseña</label>
-      <input className="form__input" type="password" id="password" name="password"
-          placeholder="Ingrese su contraseña" required />
-      <button className="form__button" type="submit">Iniciar Sesión</button>
+      <label className="form__label--required" htmlFor="email">
+        {email}
+      </label>
+      <input
+        className="form__input"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Ingrese su correo"
+        required
+      />
+      <label className="form__label--required" htmlFor="password">
+        {password}
+      </label>
+      <input
+        className="form__input"
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Ingrese su contraseña"
+        required
+      />
+      <button className="form__button" type="submit">
+        {login}
+      </button>
     </form>
-  )
-}
+  );
+};
