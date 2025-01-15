2ad9953bbca8b5f79e131b3805af58bbecb35aa2
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-15 15:19:01 -0500

    Refactor AuthHeader component to use LogoIcon and improve styles for better responsiveness

--- src/views/components/AuthHeader/index.jsx
+++ src/views/components/AuthHeader/index.jsx
@@ -1,10 +1,16 @@
+import LogoIcon from "../icons/LogoIcon";
+import "./styles.css";
 
 export const AuthHeader = () => {
+  const sloganTitle = "GestionA";
+  const sloganSubtitle = "Sistema de gestión agrícola";
+  const icon = { with: 144, height: 144, fill: "#193C32" };
+
   return (
     <aside className="container__slogan">
-      <header className="slogan__title">GestionA</header>
-      <h2 className="slogan__subtitle">Sistema de gestión agrícola</h2>
-      <img className="slogan__logo" src="../../assets/img/Logo.svg" alt="Logotipo de GestionA" />
+      <header className="slogan__title">{sloganTitle}</header>
+      <h2 className="slogan__subtitle">{sloganSubtitle}</h2>
+      <LogoIcon width={icon.with} height={icon.height} fill={icon.fill} />
     </aside>
-  )
-}
+  );
+};
