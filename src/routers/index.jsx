e4372831114d4bcc2769f1532f1d75850aa3b129
Leonardo Rincon <jleobotache@gmail.com>
2025-01-15 19:15:54 -0500

    add basic example to columns

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router';
 import { Login } from '../views/login';
 import Signup from '../views/Signup';
+import Profile from '../views/Profile';
 
 export const AppRouter = () => {
  return (
   <Routes>
    <Route path='/' element={<Login />} />
    <Route path='/signup' element={<Signup />} />
+   <Route path='/profile' element={<Profile />} />
   </Routes>
  );
 };
--- src/views/components/AdminUsers/index.jsx
+++ src/views/components/AdminUsers/index.jsx
@@ -1,52 +1,59 @@
-import "./styles.css";
+import DataTable from 'react-data-table-component';
+
+const columns = [
+ {
+  name: '#',
+  selector: (row) => row.id,
+ },
+ {
+  name: 'Nombre',
+  selector: (row) => row.nombre,
+ },
+ {
+  name: 'Correo',
+  selector: (row) => row.correo,
+ },
+ {
+  name: 'Rol',
+  selector: (row) => row.rol,
+ },
+ {
+  name: 'Permisos',
+  selector: (row) => row.permisos,
+ }
+];
+
+const data = [
+ {
+  id: 1,
+  nombre: 'Beetlejuice',
+  correo: 'example@.com',
+  rol: 'Admin',
+  permisos: 'Lector',
+ },
+ {
+  id: 2,
+  nombre: 'Beetlejuice',
+  correo: 'example@.com',
+  rol: 'Admin',
+  permisos: 'Lector',
+ },
+];
+
+import './styles.css';
 
 const AdminUser = () => {
-  return (
-    <section className="admin-user">
-      <header className="admin-user__header">
-        <h1>Administrar usuarios</h1>
-      </header>
-      <table className="admin-user__table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Rol</th>
-            <th>Permisos</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Leonardo Rincón</td>
-            <td>Example@example.com</td>
-            <td>Administrador</td>
-            <td>Lector</td>
-            <td className="actions">
-              <button className="edit-button">✏️</button>
-              <button className="delete-button">🗑️</button>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Mario Rincón</td>
-            <td>Example@example.com</td>
-            <td>User</td>
-            <td>user</td>
-            <td className="actions">
-              <button className="edit-button">✏️</button>
-              <button className="delete-button">🗑️</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <button type="button" className="add-user-button">Agregar usuario</button>
-    </section>
-  );
+ return (
+  <section className='admin-user'>
+   <header className='admin-user__header'>
+    <h1>Administrar usuarios</h1>
+   </header>
+   <DataTable columns={columns} data={data} onSelectedRowsChange={(row) => console.log(row)} selectableRows/>
+   <button type='button' className='add-user-button'>
+    Agregar usuario
+   </button>
+  </section>
+ );
 };
 
 export default AdminUser;
-
-
