a041f75ffa0cb4092ec4bdebc701dea16f7deff6
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-17 15:13:42 -0500

    refactor Project component to integrate AddProjectModal and ProjectsList, update styles

--- src/views/Project.jsx
+++ src/views/Project.jsx
@@ -1,20 +1,34 @@
 import InitialSidebar from "./components/InitialSidebar";
 import InitialView from "./components/InitialView";
-import { welcomeTexts } from "../utils/const";
+import AddProjectModal from "./components/AddProjectModal";
+import ProjectsList from "./components/ProjectsList";
+import { useRef } from "react";
 
-import "./project.css";
+import "./styles/project.css";
 
 export default function Project() {
-  const { title, descriptions } = welcomeTexts;
+  const dialogRef = useRef(null);
+  const projects = ["Proyecto Cafetera", "Aguacatera Grande"];
 
+  const handleOpenModal = () => {
+    console.log("Si se abre la modal");
+    if (dialogRef.current) dialogRef.current.showModal();
+  };
+
+  const handleCloseModal = () => {
+    console.log("si cierra");
+    if (dialogRef.current) dialogRef.current.close();
+  };
 
   return (
-    <div className="container">
+    <div className="container-project">
       <InitialSidebar />
-      <InitialView
-        title={title}
-        descriptions={descriptions}
-      />
+      {!projects.length ? (
+        <InitialView buttonOnClick={handleOpenModal} />
+      ) : (
+        <ProjectsList buttonOnClick={handleOpenModal} projects={projects} />
+      )}
+      <AddProjectModal ref={dialogRef} onClose={handleCloseModal} />
     </div>
   );
 }
