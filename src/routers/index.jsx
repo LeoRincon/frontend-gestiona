2159a2f3ee61ecf472ba7894f3ed54b680a5d041
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-17 11:13:51 -0500

    add Project route to the application router

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import { Login } from '../views/login';
 import Signup from '../views/Signup';
 import Profile from '../views/Profile';
+import Project from '../views/Project';
 
 export const AppRouter = () => {
  return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
    <Route path='/' element={<Login />} />
    <Route path='/signup' element={<Signup />} />
    <Route path='/profile' element={<Profile />} />
+   <Route path='/project' element={<Project />} />
   </Routes>
  );
 };
