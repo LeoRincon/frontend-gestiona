7d0c2b19e9c945e8fcf90b3640426bc45db0ebe9
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-17 15:11:31 -0500

    refactor AddProjectModal to use button components and improve layout

--- src/views/components/AddProjectModal/index.jsx
+++ src/views/components/AddProjectModal/index.jsx
@@ -2,38 +2,52 @@ import { forwardRef } from "react";
 import { addProjecModalTexts } from "../../../utils/const";
 
 import "./styles.css";
+import {CloseButton, PrimaryButton, SecondaryButton} from "../Buttons";
 
-const AddProjectModal = forwardRef(function AddProjectModal ({ onClose }, ref) {
+const AddProjectModal = forwardRef(({ onClose }, ref) => {
+  const {
+    title,
+    inputLabel,
+    placeholderInput,
+    confirmButtom,
+    cancelButtom,
+  } = addProjecModalTexts;
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Ingresa a la función")
-    onClose()
+    onClose();
   };
-  const handleCancel = (event)=>{
-    event.preventDefault()
-    onClose()
-  }
-
+  const handleCancel = (event) => {
+    event.preventDefault();
+    onClose();
+  };
 
   return (
     <dialog className="project-modal" ref={ref}>
-      <h2>{addProjecModalTexts.title}</h2>
-      <p>{addProjecModalTexts.description}</p>
-      <form className="project-modal__form" onSubmit={handleSubmit}>
-        <label htmlFor="project_name">{addProjecModalTexts.inputLabel}</label>
-        <input
-          type="text"
-          id="project_name"
-          name="nombre_proyecto"
-          placeholder={addProjecModalTexts.placeholderInput}
-        />
-        <button type="submit">
-          {addProjecModalTexts.confirmButtom}
-        </button>
-        <button onClick={handleCancel}>{addProjecModalTexts.cancelButtom}</button>
-      </form>
+      <CloseButton className={"btn-close"} onClick={handleCancel}/>
+      <section className="project-modal__container">
+        <h2>{title}</h2>
+        <form className="project-modal__form" onSubmit={handleSubmit}>
+          <label className="project-modal__form__label" htmlFor="project_name">
+            {inputLabel}
+          </label>
+          <input
+            className="project-modal__form__input"
+            type="text"
+            id="project_name"
+            name="nombre_proyecto"
+            placeholder={placeholderInput}
+            required
+          />
+          <div className="project-modal__form__buttons">
+            <PrimaryButton  type={"Submit"}> {confirmButtom} </PrimaryButton>
+            <SecondaryButton onClick={handleCancel}>{cancelButtom}</SecondaryButton>
+          </div>
+        </form>
+      </section>
     </dialog>
   );
 });
+
+AddProjectModal.displayName = "AddProjectModal";
 
 export default AddProjectModal;
