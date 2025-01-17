1f7cbd19ee0ea84cb265edde1348f395c95992c6
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-17 17:03:29 -0500

    refactor: update icon components to accept width, height, and fill/stroke props for better customization

--- src/views/components/icons/CaretIcon.jsx
+++ src/views/components/icons/CaretIcon.jsx
@@ -1,15 +1,14 @@
-export default function CaretIcon (props) {
+export default function CaretIcon ({ width, height, fill }) {
 
   return (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={25}
-    height={24}
+    width={width}
+    height={height}
     fill="none"
-    {...props}
   >
     <path
-      fill={props.fill}
+      fill={fill}
       fillRule="evenodd"
       d="M14.592 6.69a.72.72 0 0 1 0 1.02L10.3 12l4.29 4.29a.72.72 0 1 1-1.018 1.02l-4.8-4.8a.72.72 0 0 1 0-1.02l4.8-4.8a.72.72 0 0 1 1.019 0Z"
       clipRule="evenodd"
