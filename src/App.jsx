9561969c2df93bda4f5cd82bc146ed042a740a1d
Samuel <rodqrs@gmail.com>
2025-01-17 20:21:21 -0500

    restoring app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { Routes, Route} from "react-router";
-import { Login } from './views/login';
-import Dashboard   from './views/dashboard';
-import Information from './views/information';
-import Home from "./views/home";
-import Signup from "./views/signup";
 
+import { AppRouter } from './routers';
 import './App.css';
 
 function App() {
  return (
-    <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/information" element={<Information />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-    </Routes>);
+  <AppRouter />
+ );
 }
 
 export default App;
