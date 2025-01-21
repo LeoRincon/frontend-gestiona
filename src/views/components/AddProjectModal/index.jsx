7f358a4ac3c40866d6dac12fc4b830b9db06ea82
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-21 10:02:16 -0500

    refactor AddProjectModal to use react-hook-form for form handling and add description field

--- src/views/components/AddProjectModal/index.jsx
+++ src/views/components/AddProjectModal/index.jsx
@@ -1,46 +1,68 @@
 import { forwardRef } from "react";
 import { addProjecModalTexts } from "../../../utils/const";
+import { CloseButton, PrimaryButton, SecondaryButton } from "../Buttons";
+import { useForm } from "react-hook-form";
+import { createProject } from "../../../services/projectService";
 
 import "./styles.css";
-import {CloseButton, PrimaryButton, SecondaryButton} from "../Buttons";
 
-const AddProjectModal = forwardRef(({ onClose }, ref) => {
+const AddProjectModal = forwardRef(({ onClose, onAddElement }, ref) => {
+  const { register, handleSubmit, } = useForm();
   const {
-    title,
+    cancelButtom,
+    confirmButtom,
+    descriptionLabel,
     inputLabel,
     placeholderInput,
-    confirmButtom,
-    cancelButtom,
+    placeholderTextArea,
+    title,
   } = addProjecModalTexts;
-  const handleSubmit = (event) => {
-    event.preventDefault();
+
+  const onSubmit = async (data) => {
+    onAddElement(data);
     onClose();
   };
-  const handleCancel = (event) => {
+  const onCancel = (event) => {
     event.preventDefault();
     onClose();
   };
 
   return (
     <dialog className="project-modal" ref={ref}>
-      <CloseButton className={"btn-close"} onClick={handleCancel}/>
+      <CloseButton className={"btn-close"} onClick={onCancel} />
       <section className="project-modal__container">
         <h2>{title}</h2>
-        <form className="project-modal__form" onSubmit={handleSubmit}>
-          <label className="project-modal__form__label" htmlFor="project_name">
+        <form className="project-modal__form" onSubmit={handleSubmit(onSubmit)}>
+          <label className="project-modal__form__label" htmlFor="nombre">
             {inputLabel}
           </label>
           <input
             className="project-modal__form__input"
             type="text"
             id="project_name"
-            name="nombre_proyecto"
+            name="nombre"
             placeholder={placeholderInput}
+            {...register("nombre")}
             required
           />
+          <label className="project-modal__form__label" htmlFor="descripcion">
+            {descriptionLabel}
+          </label>
+          <textarea
+            className="project-modal__form__input"
+            type="text"
+            id="project_name"
+            name="descripcion"
+            placeholder={placeholderTextArea}
+            rows={4}
+            {...register("descripcion")}
+            required
+          ></textarea>
           <div className="project-modal__form__buttons">
-            <PrimaryButton  type={"Submit"}> {confirmButtom} </PrimaryButton>
-            <SecondaryButton onClick={handleCancel}>{cancelButtom}</SecondaryButton>
+            <PrimaryButton type={"Submit"}> {confirmButtom} </PrimaryButton>
+            <SecondaryButton onClick={onCancel}>
+              {cancelButtom}
+            </SecondaryButton>
           </div>
         </form>
       </section>
