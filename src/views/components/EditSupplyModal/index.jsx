f74b7591189dd0a7fcab3525f787bb49913871bb
anfecv <afchv09@gmail.com>
2025-01-23 14:45:58 -0500

    supply 'edit' and 'delete' service implementation

--- src/views/components/EditSupplyModal/index.jsx
+++ src/views/components/EditSupplyModal/index.jsx
@@ -1,11 +1,34 @@
+import ErrorMessageModal from '../ErrorMessageModal'
 import { useForm } from 'react-hook-form'
+import { useEffect } from 'react'
 
 import './styles.css'
 
-export default function AddSupplyModal({handleOpenModal, handleAddFetch}){
-
-  const {register, handleSubmit, formState:{errors} } =useForm() 
+export default function EditSupplyModal({id, completeData, handleEditModal, handleEditFetch}){
   
+  //SI NO SE SELECCIONADO UN REGISTRO RETORNO ESTE MENSAJE
+  if(!id){
+    return(
+      <ErrorMessageModal text="No ha selecionado ningun insumo" handleErrorModal={handleEditModal} />
+    )
+  }
+  const {register, handleSubmit, formState:{errors}, setValue } =useForm()
+
+  //SE TOMAN LOS DATOS DEL SUPPLY SELECIONADO
+  const dataValue = completeData.find((value)=>value.id === id)
+
+  //PONER DATOS POR DEFECTO EN LOS INPUTS
+  useEffect(()=>{
+    setValue("nombre",dataValue.nombre)
+    setValue("cantidad",dataValue.cantidad_disponible)
+    setValue("ingreso",dataValue.fecha_ingreso)
+    setValue("precio",dataValue.precio)
+    setValue("idInventario",dataValue.id_inventario)
+    setValue("idCategoria",dataValue.id_categoria)
+    setValue("idUnidad",dataValue.id_unidad_medida)
+  },[])
+
+
   const onSubmit= handleSubmit(
     (data)=>{
       const newData= {
@@ -17,23 +40,23 @@ export default function AddSupplyModal({handleOpenModal, handleAddFetch}){
                 "id_categoria": data.idCategoria,
                 "id_unidad_medida": data.idUnidad
               }
-      handleAddFetch(newData)
+          handleEditFetch(newData)
     } 
   )
 
   return(
     <div className="supply-popup-bg">
-      <div className="supply-popup-add">
+      <div className="supply-popup-edit">
         <header>
           <h4>
-            Ingresar Insumo
+            Editar insumo
           </h4>
           </header>
           <main>
-            <form className="supply-form-add" onSubmit={onSubmit}>
+            <form className="supply-form-edit" onSubmit={onSubmit}>
               <div>
                 <label htmlFor="nombre">Nombre</label>
-                <input type="text" name="nombre" placeholder="Ingrese el nombre del insumo" {...register("nombre",{
+                <input type="text" name="nombre" placeholder={dataValue.nombre} {...register("nombre",{
                   required:{value:true,message:"El nombre es obligatorio"},
                   minLength:{value:2,message:"El nombre debe tener al menos 2 caracteres"},
                   maxLength:{value:100,message:"El nombre no puede tener más de 100 caracteres"},
@@ -42,7 +65,7 @@ export default function AddSupplyModal({handleOpenModal, handleAddFetch}){
               </div>
               <div>
                 <label htmlFor="cantidad">Cantidad disp</label>
-                <input type="text" name="cantidad" placeholder="Ingrese la Cantidad" {...register("cantidad",{
+                <input type="text" name="cantidad" placeholder={dataValue.cantidad_disponible} {...register("cantidad",{
                   required:{value:true,message:"La cantidad es obligatoria"},
                   pattern:{value:/^[0-9]+([,][0-9]+)?$/,message:"La cantidad debe ser numerica"},
                 })} />
@@ -50,14 +73,14 @@ export default function AddSupplyModal({handleOpenModal, handleAddFetch}){
               </div>
               <div>
                 <label htmlFor="date">F. de Ingreso</label>
-                <input type="date" name="ingreso" {...register("ingreso", {
+                <input type="date" name="ingreso"  {...register("ingreso", {
                   required:{value:true,message:"La fecha es obligatoria"},
                 })} />
                 {errors.ingreso && <span>{errors.ingreso.message}</span>}
               </div>
               <div>
                 <label htmlFor="precio">Precio</label>
-                <input type="text" name="precio" placeholder="Ingrese el precio" {...register("precio", {
+                <input type="text" name="precio" placeholder={dataValue.precio} {...register("precio", {
                   required:{value:true,message:"El precio es obligatorio"},
                   pattern:{value:/^[0-9]+([,][0-9]+)?$/,message:"El precio debe ser numerico"},
                 })} />
@@ -65,7 +88,7 @@ export default function AddSupplyModal({handleOpenModal, handleAddFetch}){
               </div>
               <div>
                 <label htmlFor="inventario">Id Inventario</label>
-                <input type="text" name="inventario" placeholder="Ingrese el inventario" {...register("idInventario",{
+                <input type="text" name="inventario" placeholder={dataValue.id_inventario} {...register("idInventario",{
                   required:{value:true,message:"El campo es obligatorio"},
                   pattern:{value:/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,message:"El campo debe ser un 'uuid'"}
                 })} />
@@ -73,7 +96,7 @@ export default function AddSupplyModal({handleOpenModal, handleAddFetch}){
               </div>
               <div>
                 <label htmlFor="categoria">Id Categoria</label>
-                <input type="text" name="categoria" placeholder="Ingrese la categoria" {...register("idCategoria",{
+                <input type="text" name="categoria" placeholder={dataValue.id_categoria} {...register("idCategoria",{
                   required:{value:true,message:"El campo es obligatorio"},
                   pattern:{value:/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,message:"El campo debe ser un 'uuid'"}
                 })} />
@@ -81,7 +104,7 @@ export default function AddSupplyModal({handleOpenModal, handleAddFetch}){
               </div>
               <div>
                 <label htmlFor="unidad">Id U. de medida</label>
-                <input type="text" name="unidad" placeholder="Ingrese la unidad" {...register("idUnidad",{
+                <input type="text" name="unidad" placeholder={dataValue.id_unidad_medida} {...register("idUnidad",{
                   required:{value:true,message:"El campo es obligatorio"},
                   pattern:{value:/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,message:"El campo debe ser un 'uuid'"}
                 })} />
@@ -91,7 +114,7 @@ export default function AddSupplyModal({handleOpenModal, handleAddFetch}){
                 <button className='btn-submit'>
                   Enviar
                 </button>
-                <button className='btn-cancel' type='button' onClick={handleOpenModal}>
+                <button className='btn-cancel' type='button' onClick={handleEditModal}>
                   Cancelar
                 </button>
               </div>
@@ -100,4 +123,4 @@ export default function AddSupplyModal({handleOpenModal, handleAddFetch}){
       </div>
     </div>
   )
-}
+} 
--- src/views/components/TableSupply/index.jsx
+++ src/views/components/TableSupply/index.jsx
@@ -1,19 +1,27 @@
 import DataTable from "react-data-table-component"
 
 import "./styles.css"
-export default function TableSupply({data}){
+export default function TableSupply({data, handleSelectedData}){
+    function onSelected(row){
+      if(row.selectedRows[0]){
+        handleSelectedData(row.selectedRows[0].id)
+      }
+    }
     let dataColums = [];   
     let dataValues = [];
+    //Se comprueba que se hayan cargado las datos
     if(typeof data !== "undefined"){
       const dataTable = [...data]
+      //Se crean las datos que se usaran para "DATATABLE"
       const keys = dataTable.map((item)=>Object.keys(item))
       const values = dataTable.map((item)=>Object.values(item))
       if (keys.length > 0 && values.length > 0) {
+          //Se crean las columnas
           dataColums = keys[0].map((key) => ({
             name: key,
             selector: (row) => row[key],
           }));
-        
+          //Se crean los registros
           dataValues = values.map((value) => ({
             id: value[0],
             nombre: value[1],
@@ -28,6 +36,6 @@ export default function TableSupply({data}){
     }
 
     return(
-        <DataTable columns={dataColums} data={dataValues}  onSelectedRowsChange={(row) => console.log(row)} selectableRows/>
+        <DataTable columns={dataColums} data={dataValues}  onSelectedRowsChange={onSelected} selectableRows/>
     )
 }
