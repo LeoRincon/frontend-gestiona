ac78a795571897e5490389007f5b422935b66f55
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-25 15:02:12 -0500

    update Project component to fetch user-specific projects and pass user data to InitialSidebar

--- src/views/Project.jsx
+++ src/views/Project.jsx
@@ -1,61 +1,65 @@
-import InitialSidebar from './components/InitialSidebar';
-import InitialView from './components/InitialView';
-import AddProjectModal from './components/AddProjectModal';
-import ProjectsList from './components/ProjectsList';
-import { useEffect, useRef, useState } from 'react';
-import { createProject } from '../services/projectService';
+import InitialSidebar from "./components/InitialSidebar";
+import InitialView from "./components/InitialView";
+import AddProjectModal from "./components/AddProjectModal";
+import ProjectsList from "./components/ProjectsList";
+import { useEffect, useRef, useState } from "react";
+import { createProject, getProjectsByUserId } from "../services/projectService";
 
-import './styles/project.css';
-import { fetchProjects } from '../services/projectService';
+import "./styles/project.css";
 
 export default function Project() {
- const dialogRef = useRef(null);
- const [projects, setProjects] = useState([]);
+  const dialogRef = useRef(null);
+  const [projects, setProjects] = useState([]);
+  const [user, setUser] = useState({});
 
- useEffect(() => {
-  const dataSessionStorage = sessionStorage.getItem('user_data');
-  console.log(
-   '🇨🇴🚨 => useEffect => dataSessionStorage:',
-   JSON.parse(dataSessionStorage)
-  );
+  useEffect(() => {
+    const dataSessionStorage = sessionStorage.getItem("user_data");
+    const dataSession = JSON.parse(dataSessionStorage);
+    const { id: userId, nombre: name, email } = dataSession;
 
-  const getProjects = async () => {
-   const projects = await fetchProjects();
-   setProjects(projects);
+    setUser({ userId, name, email });
+
+    const getUserProjects = async () => {
+      const response = await getProjectsByUserId(userId);
+      setProjects(response);
+    };
+    try {
+      getUserProjects();
+    } catch (error) {
+      console.error("Error fetching userProjects", error);
+    }
+  }, []);
+
+  const handleOpenModal = () => {
+    if (dialogRef.current) dialogRef.current.showModal();
   };
-  getProjects();
- }, []);
 
- const handleOpenModal = () => {
-  if (dialogRef.current) dialogRef.current.showModal();
- };
+  const handleCloseModal = () => {
+    if (dialogRef.current) dialogRef.current.close();
+  };
+  const handelAddElement = async (element) => {
+    try {
+      const response = await createProject(element, user.userId);
+      if (!response) throw new Error("Error al crear el proyecto");
+      setProjects((prevProjects) => [...prevProjects, response]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
- const handleCloseModal = () => {
-  if (dialogRef.current) dialogRef.current.close();
- };
- const handelAddElement = async (element) => {
-  try {
-   const response = await createProject(element);
-   if (!response) throw new Error('Error al crear el proyecto');
-   setProjects((prevProjects) => [...prevProjects, response]);
-  } catch (error) {
-   console.error(error);
-  }
- };
-
- return (
-  <div className='container-project'>
-   <InitialSidebar />
-   {!projects.length ? (
-    <InitialView buttonOnClick={handleOpenModal} />
-   ) : (
-    <ProjectsList buttonOnClick={handleOpenModal} projects={projects} />
-   )}
-   <AddProjectModal
-    ref={dialogRef}
-    onClose={handleCloseModal}
-    onAddElement={handelAddElement}
-   />
-  </div>
- );
+  return (
+    <div className="container-project">
+      <InitialSidebar userData={user} />
+      {!projects.length ? (
+        <InitialView buttonOnClick={handleOpenModal} />
+      ) : (
+        <ProjectsList buttonOnClick={handleOpenModal} projects={projects} />
+      )}
+      <AddProjectModal
+        ref={dialogRef}
+        onClose={handleCloseModal}
+        onAddElement={handelAddElement}
+      />
+    </div>
+  );
 }
