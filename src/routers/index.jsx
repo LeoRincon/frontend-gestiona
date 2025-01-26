c85ed2a62bf1757d46c416286c19a4f7a0d51f21
Leonardo Rincon <jleobotache@gmail.com>
2025-01-25 21:36:23 -0500

    add the protected routes and set project id to session.

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -4,11 +4,12 @@ import Signup from '../views/Signup';
 import Profile from '../views/Profile';
 import Project from '../views/Project';
 import Home from '../views/home';
-import Dashboard   from '../views/dashboard';
+import Dashboard from '../views/dashboard';
 import Information from '../views/information';
 import Supply from '../views/supply';
-import Crop from '../views/Crop'
+import Crop from '../views/Crop';
 import Season from '../views/season';
+import { ValidateUserRoute } from './ValidateUserRoute';
 
 export const AppRouter = () => {
  return (
@@ -16,16 +17,19 @@ export const AppRouter = () => {
    <Route path='/' element={<Login />} />
    <Route path='/login' element={<Login />} />
    <Route path='/signup' element={<Signup />} />
-   <Route path='/profile' element={<Profile />} />
-   <Route path='/projects' element={<Project />} />
-   <Route path='/home' element={<Home />} />
-   <Route path='/dashboard' element={<Dashboard />} />
-   <Route path='/information' element={<Information />} />
-   <Route path='/supply' element={<Supply />} />
-   <Route path='/Crop' element={<Crop />} />
-   <Route path='/season' element={<Season />} />
+
+   <Route element={<ValidateUserRoute redirectTo={'/login'} />}>
+    <Route path='/profile' element={<Profile />} />
+    <Route path='/projects' element={<Project />} />
+    <Route path='/home' element={<Home />} />
+    <Route path='/dashboard' element={<Dashboard />} />
+    <Route path='/information' element={<Information />} />
+    <Route path='/supply' element={<Supply />} />
+    <Route path='/Crop' element={<Crop />} />
+    <Route path='/season' element={<Season />} />
+   </Route>
+
+   <Route path='/*' element={<Login />} />
   </Routes>
  );
 };
-
-
