7db2001db969329297a0c24d7c5dc5f978b57972
Juan Carlos <juancho70021@gmail.com>
2025-01-27 18:57:26 -0500

    feat: add new API paths for units, sales, and seasons

--- src/utils/const.js
+++ src/utils/const.js
@@ -45,10 +45,13 @@ export const formSignupTexts = {
 export const formLoginTexts = {
   email: "Correo",
   password: "Contraseña",
-  login: "Iniciar Sesión"
-}
+  login: "Iniciar Sesión",
+};
 export const API_URL = "http://localhost:3000/api/v1";
 export const PROJECTS_PATH = "/projects";
 export const USERS_PATH = "/users";
 export const LOGIN_PATH = "/login";
-export const CROPS_PATH = "/crops"
+export const CROPS_PATH = "/crops";
+export const UNITS_PATH = "/units";
+export const SALES_PATH = "/sales";
+export const SEASONS_PATH = "/seasons";
