1bd9bcf2d467d59e6de32c851814148770d78fb8
Juan Carlos <juancho70021@gmail.com>
2025-01-27 19:00:26 -0500

    feat: integrate InsertSalesModal and fetch sales data in Sales component

--- src/views/Sales.jsx
+++ src/views/Sales.jsx
@@ -1,19 +1,41 @@
 import "./styles/sales.css";
 import Sidebar from "./components/Sidebar";
 import SalesHeader from "./components/SalesHeader";
-import SalesBody from "./components/SalesBody";  
-
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import SalesBody from "./components/SalesBody";
+import InsertSalesModal from "./components/insertSalesModal";
+import { useRef, useEffect, useState } from "react";
+import { getSalesData } from "../services/saleService";
 
 export default function Sales() {
+  const [dataTable, setDataTable] = useState([]);
+  useEffect(() => {
+    const getSale = async () => {
+      const saleData = await getSalesData();
+      setDataTable(saleData);
+    };
+    getSale();
+  }, []);
+
+  const insertModalRef = useRef(null);
+  const handelOpenIsertModal = () => {
+    insertModalRef.current.showModal();
+  };
+
+  const handelCloseIsertModal = () => {
+    insertModalRef.current.close();
+  };
+
   return (
     <div className="home-view">
       <Sidebar />
       <main className="page-container">
         <SalesHeader />
-        <SalesBody />
+        <SalesBody
+          onClickAddButton={handelOpenIsertModal}
+          dataTable={dataTable}
+        />
       </main>
+      <InsertSalesModal onClose={handelCloseIsertModal} ref={insertModalRef} />
     </div>
   );
 }
