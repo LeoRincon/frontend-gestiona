3b0b548317f3838ba125465184543f80272b39c9
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-27 23:09:53 -0500

    refactor BackButton component for improved icon size and styling

--- src/views/components/BackButton/index.jsx
+++ src/views/components/BackButton/index.jsx
@@ -1,23 +1,19 @@
-import { NavLink } from 'react-router'; 
-import './styles.css';
+import { NavLink } from "react-router";
+import "./styles.css";
 import ArrowBackIcon from "../icons/ArrowBackIcon";
-
 
 const BackButton = () => {
   const iconProps = {
-    width: 40, // Tus valores originales
-    height: 40, // Tus valores originales
+    width: 30, // Tus valores originales
+    height: 30, // Tus valores originales
     fill: "#065e52", // Tus valores originales
   };
 
-  console.log("BackButton rendered");
   return (
-    <NavLink to='/home' className="back">
-    <ArrowBackIcon {...iconProps} />
-  </NavLink>
-  )
+    <NavLink to="/home" className="back">
+      <ArrowBackIcon {...iconProps} />
+    </NavLink>
+  );
 };
 
-
 export default BackButton;
-
