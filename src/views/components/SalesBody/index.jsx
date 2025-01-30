450e5d841eb0c4b6b04d5723ecd1a8b1e3d91a75
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-29 23:52:57 -0500

    refactor SalesBody component to enhance data display and user interaction with filters

--- src/views/components/SalesBody/index.jsx
+++ src/views/components/SalesBody/index.jsx
@@ -1,64 +1,122 @@
-import React, { useState, useEffect } from "react";
-import DeleteIcon from "../icons/DeleteIcon";
-import EditIcon from "../icons/EditIcon";
-import AddIcon from "../icons/AddIcon";
-import PopupSales from "../PopupSales";
+import { useState, useEffect } from "react";
 import DataTable from "react-data-table-component";
-import "./styles.css";
 import AddButton from "../addButton";
+import { formatingDate } from "../../../utils/formatingDate";
+import { DeleteButton, EditButton } from "../Buttons";
 
+import { getSalesData } from "../../../services/saleService";
 
-const columns = [
-  {
-    name: "#",
-    selector: (row) => row.id,
-    grow: 1,
-  },
-  {
-    name: "Temporada",
-    selector: (row) => row.temporada,
-    grow: 2,
-  },
-  {
-    name: "Fecha de Venta",
-    selector: (row) => row.fechaVenta,
-    grow: 2,
-  },
-  {
-    name: "Cantidad Vendida",
-    selector: (row) => row.cantidadVendida,
-    grow: 2,
-  },
-  {
-    name: "Unidad de Medida",
-    selector: (row) => row.unidadMedida,
-    grow: 2,
-  },
-  {
-    name: "Precio Unitario ",
-    selector: (row) => row.precioUnitario,
-    grow: 2,
-  },
-  {
-    name: "Precio Total",
-    selector: (row) => row.precioTotal,
-    grow: 2,
-  },
-  {
-    name: "Observaciones",
-    selector: (row) => row.observaciones,
-    grow: 3,
-  },
-];
+import "./styles.css";
+import ContainerItems from "../ContainerItems";
+import FilterSelect from "../FilterSelect";
+import { getSeasonsByCropId } from "../../../utils/getSeasonsByCropId";
 
-function SalesBody({ onClickAddButton,dataTable }) {
-  
+function SalesBody({
+  onClickAddButton,
+  crops,
+  handleSelectedCrop,
+  handleSelectedSeason,
+  seasons,
+  dataTable,
+  onDeleteRow,
+  onEditRow
+}) {
+  const [cropsList, setCropsList] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const columns = [
+    {
+      name: "#",
+      selector: (row) => row.id,
+      grow: 1,
+    },
+    {
+      name: "Observaciones",
+      selector: (row) => row.observaciones,
+      grow: 3,
+      wrap: true,
+    },
+    {
+      name: "Cantidad Vendida",
+      selector: (row) => row.cantidadVendida,
+      grow: 2,
+    },
+    {
+      name: "Unidad de Medida",
+      selector: (row) => row.unidadMedida,
+      grow: 2,
+      wrap: true,
+    },
+    {
+      name: "Precio Unitario ",
+      selector: (row) => row.precioUnitario,
+      grow: 2,
+    },
+    {
+      name: "Precio Total",
+      selector: (row) => row.precioTotal,
+      grow: 2,
+    },
+    {
+      name: "Fecha de Venta",
+      selector: (row) => row.fechaVenta,
+      cell: (row) => formatingDate(row.fechaVenta),
+      grow: 2,
+    },
+    {
+      name: "Acciones",
+      cell: (row) => (
+        <div>
+          <EditButton
+            title="Editar Cultivo"
+            onClick={() => onEditRow(row)}
+          />
+          <DeleteButton
+            title="Eliminar Cultivo"
+            onClick={() => onDeleteRow(row)}
+          />
+        </div>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    const getCropsList = () => {
+      const cropsNames = crops.map((crop) => crop.nombre);
+      setCropsList(cropsNames);
+    };
+    getCropsList();
+  }, [crops]);
+
+  const onSelectedSeason = (selectedSeason) => {
+    if (!show) setShow(true);
+    handleSelectedSeason(selectedSeason);
+  };
+
   return (
     <main className="sales-body">
       <div className="sales-body__actions">
-        <AddButton onClick={onClickAddButton} iconHeight={30} iconWidth={30} />
+        <ContainerItems>
+          <FilterSelect
+            title="Cultivo"
+            listOptions={cropsList}
+            handleSelectedValue={handleSelectedCrop}
+          />
+          <FilterSelect
+            title="Temporada"
+            listOptions={seasons}
+            handleSelectedValue={onSelectedSeason}
+          />
+        </ContainerItems>
+        {show && (
+          <AddButton
+            onClick={onClickAddButton}
+            iconHeight={30}
+            iconWidth={30}
+          />
+        )}
       </div>
-      <DataTable columns={columns} data={dataTable} selectableRows pagination />
+      {show && <DataTable columns={columns} data={dataTable} pagination />}
     </main>
   );
 }
