b81965caf209fb8622c4d9eee2c1093af7fd9929
anfecv <afchv09@gmail.com>
2025-01-30 16:10:34 -0500

    Supply page refactor

---
+++ src/views/components/FilterSelect2/index.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+function FilterSelect2({
+  title = "Título",
+  listOptions,
+  id,
+  handleSelectedValue,
+}) {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    if (listOptions) setList(listOptions);
+  }, [listOptions]);
+
+  const handleOnchange = (event) => {
+    handleSelectedValue({ id: event.target.value });
+  };
+
+  return (
+    <div className="filter-select">
+      <h2 className="filter-select__title">{title}</h2>
+      <div className="filter-select__container">
+        <select
+          className="filter-select__container__select"
+          name="project"
+          id={id}
+          onChange={handleOnchange}
+          defaultValue={""}
+        >
+          <option value="" disabled>
+            Selecciona una opción
+          </option>
+          {
+          list ? 
+          list.map((item, index) => (
+            <option key={index} value={item.id}>
+              {index+1}
+            </option>
+          ))
+          :
+          <option value="" disabled>
+          </option>
+          }
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default FilterSelect2;
