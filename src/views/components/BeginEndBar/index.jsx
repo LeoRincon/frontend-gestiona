3db9f06945f6bdc9bf0b49c112a4e1dee2c78ddb
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-31 17:51:49 -0500

    refactor BeginEndBar and TitleSection: update styles and integrate BackButton for improved layout and navigation

--- src/views/components/BeginEndBar/index.jsx
+++ src/views/components/BeginEndBar/index.jsx
@@ -1,11 +1,9 @@
-import BackButton from "../BackButton";
 
 import "./styles.css"
 
 export default function BeginEndBar({crop}){
     return(
         <div className="season-actions">
-            <BackButton />
             <h2 className="season-number-text">Cultivo:  {crop} </h2> 
         </div>
     )
