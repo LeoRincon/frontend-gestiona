16a2b0636a6b827288bb7fb42f3321eedc75b3b6
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-03-29 11:42:05 -0500

    Add testing setup with Vitest and configure jsdom environment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+    // hey! 👋 over here
+    globals: true,
+    setupFiles: './src/setupTest.js', // assuming the test folder is in the root of our project
+  }
 })
