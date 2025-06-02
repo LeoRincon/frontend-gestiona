9f968c88bbe3090885886e34b173a049348c46d3
Angie Molina <angiesayagom27@outlook.es>
2025-06-01 22:36:23 -0500

    se agrega tabla

--- src/views/components/ActivitiesListModal/index.jsx
+++ src/views/components/ActivitiesListModal/index.jsx
@@ -1,9 +1,24 @@
 import "./styles.css";
 import Modal from "../Modal";
 import { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
 
 const ActivitiesListModal = ({ isOpen = false, onClose }) => {
   const [openModal, setOpenModal] = useState(false);
+
+  const columns = [
+  { name: 'Nombre', selector: (row) => row.name },
+  { name: 'Descripción', selector: (row) => row.description },
+  { name: 'Categoria', selector: (row) => row.category },
+];
+
+const data = [
+  { name: 'unooo', description: 'aaaaaaaaaa', category: 'Admin' },
+  { name: 'dosssss', description: 'bbbbbbbbbbb', category: 'Admin' },
+  { name: 'tressss', description: 'ccccccccccc', category: 'Admin' },
+  { name: 'cuatrooo', description: 'ffffffff', category: 'Admin' },
+  { name: 'cincooo', description: 'rrrrrrrrrr', category: 'Admin' }
+];
 
   useEffect(()=>{
     if (isOpen)
@@ -30,7 +45,8 @@ const ActivitiesListModal = ({ isOpen = false, onClose }) => {
         <h2 className="activities-list-modal__title">Actividades</h2>
       </header>
       <main>
-        {/* TODO: Crear la tabla de actividades */}
+        
+        <DataTable columns={columns} data={data} />
 
         {/* TODO: Crear el formulario para añadir actividades */}
       </main>
